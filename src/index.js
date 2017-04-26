f4429310317295e8a9f084342ca6ceb3b5153464
chrislearn <chris@acroidea.com>
2017-04-26 11:36:05 +0800

    u

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * from './widgets'
 export stores, {getStore, addStores, factory as storeFactory} from './stores'
-export {config as URLConfig, getAPILoginURL, getUILoginURL, getUIForbiddenURL} from './url'
+export {config as URLConfig, getAPILoginURL, getUILoginURL, getUIForbiddenURL, getUIURL, getAPIURL} from './url'
 export client from './client'
