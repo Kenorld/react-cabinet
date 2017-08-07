01f1f65018f8155d2bd959a651f9cd9776dae898
chrislearn <chris@acroidea.com>
2017-08-07 09:26:43 +0800

    Edit button label and action

--- src/widgets/button/EditButton.js
+++ src/widgets/button/EditButton.js
@@ -5,17 +5,23 @@ import FlatButton from 'material-ui/FlatButton';
 import ContentCreate from 'material-ui/svg-icons/content/create';
 import {getUIURL} from '../../url'
 
-const EditButton = ({ entityName = '', record = {} }) => <FlatButton
+const EditButton = ({ entityName = '', record = {}, label, action }) => <FlatButton
     primary
-    label="Edit"
+    label={label}
     icon={<ContentCreate />}
-    containerElement={<Link to={getUIURL(entityName, 'edit', record.id)} />}
+    containerElement={<Link to={getUIURL(entityName, action, record.id)} />}
     style={{ overflow: 'inherit' }}
 />;
 
 EditButton.propTypes = {
     entityName: PropTypes.string,
     record: PropTypes.object,
+    label: PropTypes.string,
+    action: PropTypes.string,
 };
+EditButton.defaultProps = {
+    label: 'Edit',
+    action: 'edit'
+}
 
 export default EditButton;
